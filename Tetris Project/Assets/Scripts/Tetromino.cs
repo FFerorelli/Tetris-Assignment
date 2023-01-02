12c7d5ed5d78a0f6ab007d9bb7337ed699fd519f
FFerorelli <fabrizio.ferorelli@gmail.com>
2023-01-02 14:25:53 +0100

    Tetrominos Data

--- Tetris Project/Assets/Scripts/Tetromino.cs
+++ Tetris Project/Assets/Scripts/Tetromino.cs
@@ -11,6 +11,14 @@ public struct TetrominoData
 {
     public Tile tile;
     public Tetromino tetromino;
+
     public Vector2Int[] cells { get; private set; }
+    public Vector2Int[,] wallKicks { get; private set; }
+
+    public void Initialize()
+    {
+        cells = Data.Cells[tetromino];
+        wallKicks = Data.WallKicks[tetromino];
+    }
 
 }
